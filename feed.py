70721f1f2e5ac4163cfef031835171ef76076305
monty-nabil <monty.slovakia@gmail.com>
2024-12-05 19:36:55 +0000

    fix typo

--- feed.py
+++ feed.py
@@ -7,7 +7,7 @@ with open("feed.yaml", "r") as file:
     rss_element = xml_tree.Element(
         "rss",{
             "version": "2.0",
-            "xmlns:itunes": "http://www. itunes. com/dtds/podcast-1.0.dtd",
+            "xmlns:itunes": "http://www.itunes.com/dtds/podcast-1.0.dtd",
             "xmLns:content": "http://purl.org/rss/1.0/modules/content/",
         },
     )
